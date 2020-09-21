b0babcccc3d9d18f694507c2e752b8dcb5630b1d
Chelsey Blair Masood <58539738+devgirl221@users.noreply.github.com>
2020-09-20 22:11:13 -0500

    controllers

---
+++ controllers/login-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Game, User, Vote } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    console.log(req.session)
+    console.log('---------------')
+    Game.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'platform',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE game.id = vote.game_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbGameData => {
+            const games = dbGameData.map(game => game.get({ plain: true}))
+            res.render('dashboard', { games, loggedIn: true})
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+module.exports = router
