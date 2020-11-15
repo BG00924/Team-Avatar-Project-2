f1bf84642fb10263034dc5b2abbcd2d7a24bc6cf
BG00924 <BG00924@users.noreply.github.com>
2020-11-15 17:08:54 -0600

    mvp

--- public/javascript/mygames.js
+++ public/javascript/mygames.js
@@ -1,30 +1,28 @@
-
-var deleteGameBtn = document.getElementById('delete-btn')
-
-var handleDeleteSubmit = function(event) {
-    event.preventDefault()
-
-     var Id = event.target.parentNode.id
-
-    fetch(`/api/games/${Id}`, {
-        method: 'DELETE'
-      });
-    
-    document.location.reload('/mygames')
+// var deleteGameBtn = document.getElementById('delete-btn')
+var elements = document.getElementsByClassName("delete-btn");
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('click', handleDeleteSubmit);
 }
-
-
+function handleDeleteSubmit(event) {
+  event.preventDefault()
+  event.stopPropagation()
+  var Id = event.target.parentNode.id
+  fetch(`/api/games/${Id}`, {
+    method: 'DELETE'
+  });
+  document.location.reload('/mygames')
+}
 // bulma modal javascript 
 var refs = {
-    modalEdicion: {
-      open: function() { document.getElementById('modalEdicion').classList.add('is-active');
-      },
-      close:function() { document.getElementById('modalEdicion').classList.remove('is-active');
-                        
-      }
+  modalEdicion: {
+    open: function () {
+      document.getElementById('modalEdicion').classList.add('is-active');
+    },
+    close: function () {
+      document.getElementById('modalEdicion').classList.remove('is-active');
     }
-  };
+  }
+};
 // bulma modal end
-
 // Event listeners
-deleteGameBtn.addEventListener("click", handleDeleteSubmit);
+// deleteGameBtn.addEventListener("click", handleDeleteSubmit);
